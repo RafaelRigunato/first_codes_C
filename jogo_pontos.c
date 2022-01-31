2e6932ad1d0dd8103a22fa0a7ed5241dc50646c4
RafaelRigunato <96690017+RafaelRigunato@users.noreply.github.com>
2022-01-30 23:46:21 -0300

    jogo_pontos.c
    Durante um acampamento, um grupo de amigos decidiu criar um jogo de cartas, chamado Jogo dos pontos. Em razão dos poucos recursos que tinham para elaborar as cartas, foi decidido que estas teriam um número inteiro em sua face e nada mais. Esse número representa o total de pontos que o detentor dela possui, sendo um valor entre -10 e 10.
    
    Este jogo deve ser conduzido com três jogadores e possui duas rodadas. O jogo inicia com o monte de cartas com suas faces viradas para baixo. Na primeira rodada, os jogadores deverão cada um em sua vez, pegar as três cartas do topo. No término da primeira rodada, todos os jogadores deverão necessariamente estar com três cartas de diferentes valores em suas mãos.
    
    Na segunda rodada, a primeira pessoa a pegar as cartas deverá solicitar a primeira carta que o segundo jogador pegou do monte. Dessa forma, nesse momento, o primeiro jogador estará com quatro cartas na mão, e o segundo jogador estará com apenas duas. Nesse instante, o segundo jogador deverá solicitar a segunda carta da terceira pessoa. Agora, o segundo jogador possui em suas mãos o total de três cartas e o jogador três possui apenas duas. Para equilibrar o número de cartas entre todos os jogadores, o terceiro e último jogador, por fim, pega a terceira carta do primeiro jogador.
    
    O vencedor do Jogo dos pontos é aquele cuja soma dos valores das cartas em suas mãos seja o maior valor da mesa.
    
    Sua tarefa é elaborar um programa em linguagem C, que receba a sequência dos valores das cartas de cada jogador na primeira rodada. Sendo assim, seu programa deverá receber nove valores inteiros. O primeiro valor recebido é referente à primeira carta do primeiro jogador. E assim sucessivamente até receber o valor da terceira carta do terceiro jogador. Em seguida, deverá processar a segunda rodada e apresentar o total de pontos nas mãos dos três jogadores.

---
+++ jogo_pontos.c
@@ -0,0 +1,28 @@
+int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    int h;
+    int i;
+    int j;
+    int resultado_1;
+    int resultado_2;
+    int resultado_3;
+    
+    scanf("%d %d %d %d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+    
+    resultado_1 = a + b + d;
+    resultado_2 = e + f + h;
+    resultado_3 = g + i + c;
+    
+    printf("%d\n", resultado_1);
+    printf("%d\n", resultado_2);
+    printf("%d\n", resultado_3);
+    
+    
+    
+    return 0;
+}
