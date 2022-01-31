a3e368b3721169f1ce5949424cc161b60b2aee95
RafaelRigunato <96690017+RafaelRigunato@users.noreply.github.com>
2022-01-30 22:59:04 -0300

    Create numero_dias.c
    Nesse exercício, você deverá implementar um algoritmo que calcule o total de dias de n meses. Para isso, seu programa deve receber um valor inteiro, representando o total de meses. Em seguida, deverá calcular e imprimir o resultado . Considere que todos os meses tenham 30 dias.

---
+++ numero_dias.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+    
+    int numero_mes = 0;
+    int numero_dias = 0;
+    
+    printf(" informe o número de meses ");
+    scanf("%d", &numero_mes);
+    
+    numero_dias = numero_mes * 30;
+    
+    printf(" os meses informados contém: %d", numero_dias);
+    
+    
+    return 0;
+    
+}
